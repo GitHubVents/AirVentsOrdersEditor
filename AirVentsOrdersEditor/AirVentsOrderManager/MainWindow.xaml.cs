f032310faf986261b32b54c0a3dc2e4de4380ef7
DenisOrel <4uzhoy>
2015-05-21 15:40:10 +0300

    1

--- AirVentsOrdersEditor/AirVentsOrderManager/MainWindow.xaml.cs
+++ AirVentsOrdersEditor/AirVentsOrderManager/MainWindow.xaml.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace AirVentsOrderManager
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
@@ -40,7 +29,7 @@ namespace AirVentsOrderManager
 
         private void EditManager_Click(object sender, RoutedEventArgs e)
         {
-            var editManagerWindow = new Window {};
+            var editManagerWindow = new Window();
             editManagerWindow.ShowDialog();
         }
     }
