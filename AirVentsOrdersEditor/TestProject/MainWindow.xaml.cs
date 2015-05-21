3d063ce40e32b4d49d78f7f2ec301a395a19c09b
Николай Антонюк <antomykl@gmail.com>
2015-05-21 13:28:48 +0300

    c59b065bec4d75e0678a51e0ba675bdaf019d377

---
+++ AirVentsOrdersEditor/TestProject/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using OrdersRegistration;
+
+namespace TestProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           //var pdmFilesFolders = 
+
+            new PdmFilesFolders
+            {
+                OrderName = "AV05 788 6B.sldasm",
+                VaultName = "Tets_debag", //"Vents-PDM",
+            }.CreateOrder();
+
+            //try
+            //{
+              
+            //}
+            //catch (Exception exception)
+            //{
+            //    MessageBox.Show(exception.StackTrace);
+            //}
+               
+        }
+    }
+}
