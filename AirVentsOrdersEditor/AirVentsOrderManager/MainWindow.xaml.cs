1344c2e56cde2d77b68724f608d8840b5a6ee16a
Николай Антонюк <antomykl@gmail.com>
2015-05-21 15:21:01 +0300

    qefg

--- AirVentsOrdersEditor/AirVentsOrderManager/MainWindow.xaml.cs
+++ AirVentsOrdersEditor/AirVentsOrderManager/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace AirVentsOrderManager
 {
@@ -30,7 +17,7 @@ namespace AirVentsOrderManager
 
         private void NewOrder_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("ljhlj");
         }
 
         private void ExportToExel_Click(object sender, RoutedEventArgs e)
